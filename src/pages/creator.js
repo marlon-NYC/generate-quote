4f0bbdee941b7d045a206c46165d964dcd1da381
Marlon Pimentel <pimentelmarlon06@gmail.com>
2023-02-19 17:36:49 -0500

    new CSS. Refactored to new component

--- src/pages/creator.js
+++ src/pages/creator.js
@@ -3,9 +3,15 @@ import Link from 'next/link';
 const Bio = () => 
 (
    
-    <div>
-         <h1>Me</h1>
-        <Link href="/">Main Page</Link>
+    <div className='container2'>
+         <h1>Who Am I?</h1>
+         <div id="bioContainer">My name is Marlon Pimentel and I am a Software Engineer.
+         I am a man of many talents, many skills. Both a creative and a visionary.
+         Challenges interest me. Beyond my drive to understand things, I love
+         to create through different ways. This app is simply one of them.</div>
+         <div className="linkContainer">
+            <Link href="/">Main Page</Link>
+        </div>
     </div>
 );
 
